bc9a72cdf53521c0bc12f4255cfb2204849955df
Hannatu Abdullah <hannatu.abdullah@gaiacademy.org>
2025-08-12 16:44:32 +0000

    Updated user_input.py

--- user_input.py
+++ user_input.py
@@ -1,14 +1,12 @@
-# 1. GETTING INPUT FROM USERS
-
 name = input("Enter your name: ")
 #print("Hello, " + name + "!")
 
 myName = "Hannatu"
 surName = "Abdullah"
-Full_Name = myName + " " + surName  # string concatenation is the process of joining two strings together
+Full_Name = myName + " " + surName
 print("Full Name: " + Full_Name)
 
 text = "Learning Python is fun!"
-print(text.upper())  # Convert text to uppercase
-print(text.lower())  # Convert text to lowercase
-print(len(text))  # Get the length of the text
+print(text.upper()) 
+print(text.lower())  
+print(len(text))  
